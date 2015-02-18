f05b7fd84587417366e13545924045ff71b2d09f
BuildTools <unconfigured@null.spigotmc.org>
2015-02-18 13:12:02 +0100

    Init

---
+++ StringsH7 (1)/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADDLL;
+
+namespace StringsH7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int threadCount = 0;
+            Process proc = Process.GetCurrentProcess();
+            foreach (ProcessThread pt in proc.Threads)
+            {// Set all our threads to use core 1 of the CPU
+                pt.IdealProcessor = 0;
+                pt.ProcessorAffinity = (IntPtr)0x1;
+                pt.PriorityLevel = ThreadPriorityLevel.Highest;
+                threadCount++;
+            }
+
+            Console.WriteLine("> Thread affinity changes: {0}", threadCount);
+
+            object threadLock = new object(); 
+ 
+            //Size of the string
+            int size = 10000;
+            
+            //Timer for the StringBuilder
+            HighResolutionTimer timerSB = new HighResolutionTimer(true);
+            //Timer for the string
+            HighResolutionTimer timerString = new HighResolutionTimer(true);
+
+            // The order of magnitude we're displaying our times in
+            TimeResolution timeResolution = TimeResolution.Milliseconds;
+
+            // Helper variables
+            double SBDuration = 0, SDuration = 0;
+ 
+            //Output settings
+            Console.WriteLine("> size: {0}, TimeResolution: {1} \n", 
+                size, timeResolution.ToString());
+
+            Console.Write("> Stopwatch: ");
+            lock (threadLock)
+            {
+                // Lock the thread
+                timerSB.Start();
+                BuildSB(size);
+                    
+                timerSB.Stop();
+                SBDuration = timerSB.Duration(timeResolution);
+            }
+
+            lock (threadLock)
+            {
+                // Lock the thread
+                timerString.Start();
+                BuildString(size);
+
+                timerString.Stop();
+                SDuration = timerString.Duration(timeResolution);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Duration " + SDuration + " " + SBDuration);
+
+            Console.WriteLine("> String Duration: {0} {1}", 
+                SDuration, timeResolution.ToString());
+
+            Console.WriteLine("> StringBuilder Duration: {0} {1}", 
+                SBDuration, timeResolution.ToString());
+
+            // Calculate percentage
+            double highest = Math.Max(SBDuration, SDuration);
+            double lowest = Math.Min(SBDuration, SDuration);
+
+            Console.WriteLine("Verschil: " + (highest - lowest));
+        
+            Console.ReadLine();
+        }
+
+        /*
+         * Static function that build strings with the StringBuilder class
+         */
+        static void BuildSB(int size)
+        {
+            StringBuilder sbObject = new StringBuilder();
+            for (int i = 0; i <= size; i++)
+            {
+                sbObject.Append("a");
+            }
+
+          //  Console.WriteLine(sbObject.ToString());
+        }
+
+        /*
+         * Static function that builds strings with the string class
+         */
+        static void BuildString(int size) 
+        {
+            string stringObject = "";
+            for (int i = 0; i <= size; i++)
+            {
+                stringObject += "a";
+               // Console.WriteLine(stringObject.ToString());
+            }
+        }
+
+        /*
+         * Test to use the StringBuilder
+         */
+        public static void TestStringBuilder()
+        {
+            StringBuilder stBuff = new StringBuilder("Ken Thompson");
+            Console.WriteLine("Length of stBuff3: " + stBuff.Length);
+            Console.WriteLine("Capacity of stBuff3: " + stBuff.Capacity);
+            Console.WriteLine("Maximum capacity of stBuff3: " +
+            stBuff.MaxCapacity);
+            Console.Read();
+        }
+    }
+}
--- StringsH7/Program.cs
+++ StringsH7/Program.cs
@@ -1,152 +1,140 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using ADDLL;
 
 namespace StringsH7
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        //Fields:
+        private static int threadCount;
+        private static readonly Process proc = Process.GetCurrentProcess();
+        private static readonly Object threadLock = new object();
+        //Size of the string
+        //int size = 100000;
+        private static readonly int size = 100;
+        //Timer for the StringBuilder
+        private static readonly HighResolutionTimer timerSB = new HighResolutionTimer(true);
+        //Timer for the string
+        private static readonly HighResolutionTimer timerString = new HighResolutionTimer(true);
+        // The order of magnitude we're displaying our times in
+        private static readonly TimeResolution timeResolution = TimeResolution.Milliseconds;
+        // Helper variables
+        private static double SBDuration, SDuration;
+        //Methods:
+        private static void Main(string[] args)
         {
-            int threadCount = 0;
-            Process proc = Process.GetCurrentProcess();
+            WriteIntroduction();
+            CPUWarmUp();
+            SetAffinity();
+            RunTest();
+        }
+
+        private static void WriteIntroduction()
+        {
+            Console.WriteLine("> **********************************");
+            Console.WriteLine("> *****  Timing test by INF2G  *****");
+            Console.WriteLine("> *****  Version 0.1           *****");
+            Console.WriteLine("> **********************************");
+            Console.WriteLine();
+        }
+
+        private static void RunTest()
+        {
+            lock (threadLock)
+            {
+                // Lock the thread
+                timerSB.Start();
+                BuildSB(size);
+
+                timerSB.Stop();
+                SBDuration = timerSB.Duration(timeResolution);
+            }
+
+            lock (threadLock)
+            {
+                // Lock the thread
+                timerString.Start();
+                BuildString(size);
+
+                timerString.Stop();
+                SDuration = timerString.Duration(timeResolution);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("> String Duration: {0} {1}",
+                SDuration, timeResolution);
+
+            Console.WriteLine("> StringBuilder Duration: {0} {1}",
+                SBDuration, timeResolution);
+
+            // Calculate percentage
+            var highest = Math.Max(SBDuration, SDuration);
+            var lowest = Math.Min(SBDuration, SDuration);
+
+            Console.WriteLine("> Difference: " + (highest - lowest));
+
+            Console.ReadLine();
+        }
+
+        private static void SetAffinity()
+        {
             foreach (ProcessThread pt in proc.Threads)
-            {// Set all our threads to use core 1 of the CPU
+            {
+                // Set all our threads to use core 1 of the CPU
                 pt.IdealProcessor = 0;
-                pt.ProcessorAffinity = (IntPtr)0x1;
+                pt.ProcessorAffinity = (IntPtr) 0x1;
                 pt.PriorityLevel = ThreadPriorityLevel.Highest;
                 threadCount++;
             }
 
             Console.WriteLine("> Thread affinity changes: {0}", threadCount);
-
-            object threadLock = new object(); 
- 
-            //Size of the string
-            int size = 10000;
-
-            // The size of the array we're sorting
-            int arraySize = 3;
-            
-            //TimerSB(TimerStringBuilder)
-            HighResolutionTimer timerSB = new HighResolutionTimer(true);
-            //TimerSB(TimerString)
-            HighResolutionTimer timerString = new HighResolutionTimer(true);
-
-            // The order of magnitude we're displaying our times in
-            TimeResolution timeResolution = TimeResolution.Milliseconds;
-
-            // Helper variables
-            int tenPercent = size / 10;
-            double SBAverage = 0, SBDuration = 0;
-            double SAverage = 0, SDuration = 0;
-
-            //Output settings
-            Console.WriteLine("> ArraySize: {0}, size: {1}, TimeResolution: {2} \n", 
-                arraySize, size, timeResolution.ToString());
-
-            Console.Write("> Stopwatch: ");
-            lock (threadLock)
-            {
-                // Lock the thread
-                for (int i = 0; i <= arraySize; i++)
-                {
-                    timerSB.Start();
-                    BuildSB(size);
-                    
-                    timerSB.Stop();
-                    SBDuration = timerSB.Duration(timeResolution);
-                    if (i % tenPercent == 0)
-                    {// Display some progress
-                        Console.Write("{0}% ",  ((i / tenPercent) * 10));
-                    }            
-                }
-                Console.Write("100%");
-            }
-
-            lock (threadLock)
-            {
-                // Lock the thread
-                for (int i = 0; i <= arraySize; i++)
-                {
-                    timerString.Start();
-                    BuildString(size);
-
-                    timerString.Stop();
-                    SDuration = timerString.Duration(timeResolution);
-                    if (i % tenPercent == 0)
-                    {// Display some progress
-                        Console.Write(" {0}% ", ((i / tenPercent) * 10));
-                    }
-                }
-                Console.Write("100%");
-            }
-
-            Console.WriteLine();
-            Console.WriteLine();
-
-            Console.WriteLine("Duration " + SDuration + " " + SBDuration);
-            SAverage = (SDuration / size);
-
-            Console.WriteLine("> String Average Duration: {0} {1}", 
-                SAverage, timeResolution.ToString());
-
-            SBAverage = (SBDuration/ size);
-            Console.WriteLine("> StringBuilder Average Duration: {0} {1}", 
-                SBAverage, timeResolution.ToString());
-
-            // Calculate percentage
-            double highest = Math.Max(SAverage, SBAverage);
-            double lowest = Math.Min(SAverage, SBAverage);
-
-            Console.WriteLine("Verschil: " + (highest - lowest));
-        
-            Console.ReadLine();
         }
 
         /*
          * Static function that build strings with the StringBuilder class
          */
-        static void BuildSB(int size)
+
+        private static void BuildSB(int size)
         {
-            StringBuilder sbObject = new StringBuilder();
-            for (int i = 0; i <= size; i++)
+            var sbObject = new StringBuilder();
+            for (var i = 0; i <= size; i++)
             {
                 sbObject.Append("a");
             }
-
-          //  Console.WriteLine(sbObject.ToString());
         }
 
         /*
          * Static function that builds strings with the string class
          */
-        static void BuildString(int size) 
+
+        private static void BuildString(int size)
         {
-            string stringObject = "";
-            for (int i = 0; i <= size; i++)
+            var stringObject = "";
+            for (var i = 0; i <= size; i++)
             {
                 stringObject += "a";
-               // Console.WriteLine(stringObject.ToString());
             }
         }
 
-        /*
-         * Test to use the StringBuilder
-         */
-        public static void TestStringBuilder()
+        /// <summary>
+        ///     Feature to stabilize CPU clockspeed.
+        ///     Many modern processors have power-saving features
+        ///     that decrease clockspeed when cpu-utilization is low.
+        ///     This method maximises cpu clockspeed prior to running the timing test.
+        /// </summary>
+        public static void CPUWarmUp()
         {
-            StringBuilder stBuff = new StringBuilder("Ken Thompson");
-            Console.WriteLine("Length of stBuff3: " + stBuff.Length);
-            Console.WriteLine("Capacity of stBuff3: " + stBuff.Capacity);
-            Console.WriteLine("Maximum capacity of stBuff3: " +
-            stBuff.MaxCapacity);
-            Console.Read();
+            Console.Write("> Warming up CPU...");
+            for (var i = 0; i < 1000000000; i++)
+            {
+                var val1 = Math.PI*Math.PI;
+                var val2 = val1*(Math.PI/122);
+            }
+            Console.WriteLine("Done.");
+            Console.WriteLine();
         }
     }
 }
